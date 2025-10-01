0bf13bbaa64ee5fc61c6d403adf3aa4de4924c34
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-10-01 16:20:44 -0600

    Content of sesion 7

--- examples/sesion7/app.py
+++ examples/sesion7/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return "¡Hola, mundo desde Flask!"
 
+@app.route("/saludo")
+def saludo():
+    return "Bienvenido a la página de saludo."
+
+@app.route("/contacto")
+def contacto():
+    return "Página de contacto."
+
 if __name__ == "__main__":
     app.run(debug=True)
